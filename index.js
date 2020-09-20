1f09aa337b89924e4765b069e82b52a2b4d398be
Johg <park2light@gmail.com>
2020-09-20 15:26:43 +0900

    로그인까지 오류없이잘됨

--- index.js
+++ index.js
@@ -5,14 +5,17 @@ const app = express()
 const port = 5000
 const {User} = require("./models/User")
 const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
 const mongoose = require('mongoose')
-
+const config = require('./config/key')
 //application/x-www-form-urlencoded 이렇게 된것을 분석해서 가져옴
 app.use(bodyParser.urlencoded({extended:true}));
 //application/json 으로된것을 분석해서 가져올수있게함
 app.use(bodyParser.json());
+app.use(cookieParser());
 
-mongoose.connect('mongodb+srv://psh:psh1234@boilerplate.idmdn.mongodb.net/boilerplate?retryWrites=true&w=majority',{
+//mongoose.connect(config.mongoURI,{
+  mongoose.connect('mongodb+srv://psh:psh1234@boilerplate.idmdn.mongodb.net/boilerplate?retryWrites=true&w=majority',{
     useNewUrlParser: true, useUnifiedTopology:true,useCreateIndex:true,useFindAndModify:false
 }).then(() =>console.log('MongoDB Connected!'))
     .catch(err=> console.log(err))
@@ -21,7 +24,7 @@ mongoose.connect('mongodb+srv://psh:psh1234@boilerplate.idmdn.mongodb.net/boiler
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!~~')
 })
 
 app.post('/register',(req,res)=>{
@@ -32,12 +35,54 @@ app.post('/register',(req,res)=>{
     const user = new User(req.body)
     //정보들이 user모델이 저장됨
     //db에저장
+
+
     user.save((err,userInfo)=>{
         if(err) return res.json({ success:false,err})
         return res.status(200).json({success:true})
     }) 
 
 })
+
+app.post('/login',(req,res)=>{
+
+  //요청된 이메일을 데이터 베이스에서 있는지 찾는다
+  
+  User.findOne({ email:req.body.email},(err,user)=>{
+    if(!user){
+      return res.json({
+        loginSuccess:false,
+        message:"제공된 이메일에 해당하는 유저가 없습니다. "
+      })
+    }
+     //요청된 이메일이 데이터베이스에있다면 비밀번호가 맞는비밀번호인지 확인
+     user.comparePassword( req.body.password,(err,isMatch)=>{
+        if(!isMatch)
+        return res.json({loginSuccess:false,message:"비밀번호가 틀렸습니다."})
+
+        //비밀번호가 맞다면 토큰을 생성한다. 원래 여기가 좀 복잡하다
+        //토큰 생성을 위해 jsonwebtoken이라는 라이브러리를 설치
+        user.generateToken((err,user)=>{
+            if(err) return res.status(400).send(err);
+            // 토큰을 저장한다. 어디에? user에 토큰이 현재 들어있다. 쿠키 ,로컬스토리지에 저장할수있는데 장단점이있다
+             // 여기서는 쿠키에 저장한다. 
+             //쿠키에 x_auth 란 이름으로 들어감
+            res.cookie("x_auth",user.token)
+            .status(200)
+            .json({ loginSuccess:true,userId:user._id })
+           
+        })
+     })
+
+  })
+
+ 
+
+
+})
+
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
--- models/User.js
+++ models/User.js
@@ -1,4 +1,8 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken');
+//salt가 10자리
+const saltRounds = 10
 const userSchema = mongoose.Schema({
     name:{
         type:String,
@@ -29,9 +33,65 @@ const userSchema = mongoose.Schema({
     }
 
 })
+
+userSchema.pre('save',function( next ){
+//이 안에 할꺼 다한다음에 이게 끝나면은 next로 넘어감
+//비밀번호를 암호화 시킨다.
+var user= this; // 지금 이페이지를 가르킴
+if(user.isModified('password')){
+bcrypt.genSalt(saltRounds,function(err,salt){
+    if(err) return next(err)
+ 
+       //이메일같은게 아니라 비밀번호만 바꿀때
+        bcrypt.hash(user.password,salt,function(err,hash){
+            if(err) return next(err)
+            //user.password 를 hash된 비밀번호로 바꿔줌
+            user.password = hash
+            next() // index /register로돌아감
+        })
+    })
+        
+    }else{
+        next()
+    }
+})
+
+userSchema.methods.comparePassword =function(plainPassword,cb){
+    //plainPassword 1234567 암호화된 비밀번호 #$#*FHKLWsdfsdfs
+    //암호화를 한다음에 맞는지 체크해야함
+    bcrypt.compare(plainPassword,this.password,function(err,isMatch){
+            //비교해봐서 같으면 콜백
+        if (err) return cb(err);
+        //이게 user.comparePassword(req.body~~)로 다시감
+        cb(null, isMatch);
+       
+       // if(err) return cb(err),
+        //cb(null,isMatch) 
+    })
+
+}
+
+
+userSchema.methods.generateToken = function(cb){
+
+    var user = this;
+    //jsonwebToken을 이용해서 토큰 생성하기
+
+    var token = jwt.sign(user._id.toHexString(),'secretToken')
+
+    // user._id +'secretToken' = token
+    // ->
+    // 'secretToken' -> user._id
+    user.token = token
+    user.save(function(err,user){
+        if(err) return cb(err)
+        //save가 잘 됐으면은 에러는 없고 user정보만 잘 전달해주면됨
+        cb(null,user)
+        //이 user 정보가 user.generateToken(req.body) 으로 넘어감
+    })
+}
+
+
+
 const User = mongoose.model('User',userSchema) //스키마를 모델로 감싸줌
 module.exports ={User}//이 모델을 다른파일에서도 쓸수있게 exprot 해줌
-//시작할떄 git init 해도 stagingArea로 다 들어가고 추가로 더하고싶으면 git add .
-//git add . 하면 workingDirectory에 있떤 파일들을 StagingArea로 옮겨줌 깃에 넣기전에 대기시켜두는곳
-//git rm --cached node_modules -r 하면 stagingArea에 있는 것들을 지워줌
-// git commit -m "또저장소에 올림" 이파일들이 git repository(Local)에 올라감 그럼 이제 stagingArea에 아무것도 없음
